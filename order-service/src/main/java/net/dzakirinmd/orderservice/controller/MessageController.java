f9b95cc841f243785f658c5f6de576c9d3075c9b
Dzakirin <m.abdrahim@dxc.com>
2022-08-12 12:22:30 +0800

    Added REST API for Producer

--- order-service/src/main/java/net/dzakirinmd/orderservice/controller/MessageController.java
+++ order-service/src/main/java/net/dzakirinmd/orderservice/controller/MessageController.java
@@ -1,9 +1,34 @@
 package net.dzakirinmd.orderservice.controller;
 
+import net.dzakirinmd.basedomains.dto.OrderDto;
+import net.dzakirinmd.basedomains.dto.OrderEventDto;
+import net.dzakirinmd.orderservice.kafka.producer.OrderProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(name = "/api/v1/order-service")
 public class MessageController {
+
+    @Autowired
+    private OrderProducer orderProducer;
+
+    @PostMapping("/orders")
+    public String placeOrder(OrderDto orderDto) {
+
+        orderDto.setOrderId(UUID.randomUUID().toString());
+
+        OrderEventDto orderEventDto = new OrderEventDto();
+        orderEventDto.setStatus("PENDING");
+        orderEventDto.setMessage("Order status is in pending state");
+        orderEventDto.setOrderDto(orderDto);
+
+        orderProducer.sendMessage(orderEventDto);
+
+        return "Order placed sucessfully ...";
+    }
 }
