15d351c12dfaea2c3f30bb6873251eb797dc33cc
Dzakirin <m.abdrahim@dxc.com>
2022-08-15 14:45:30 +0800

    Added email service

--- email-service/src/main/java/net/dzakirinmd/emailservice/kafka/consumer/EmailConsumer.java
+++ email-service/src/main/java/net/dzakirinmd/emailservice/kafka/consumer/EmailConsumer.java
@@ -1,4 +1,4 @@
-package net.dzakirinmd.stockservice.kafka.consumer;
+package net.dzakirinmd.emailservice.kafka.consumer;
 
 import net.dzakirinmd.basedomains.dto.OrderEventDTO;
 import org.slf4j.Logger;
@@ -7,18 +7,18 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderConsumer {
+public class EmailConsumer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailConsumer.class);
 
     @KafkaListener(
             topics = "${spring.kafka.topic.name}",
             groupId = "${spring.kafka.consumer.group-id}"
     )
     public void consumeOrder(OrderEventDTO orderEventDto) {
-        LOGGER.info(String.format("Order Event received in stock-service => %s" , orderEventDto.toString()));
+        LOGGER.info(String.format("Order Event received in email-service => %s" , orderEventDto.toString()));
 
-        // TODO save the order event into the database
+        // TODO send email to customer
 
     }
 
